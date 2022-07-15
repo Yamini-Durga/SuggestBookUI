12e48b277713f62c849646fecf333612cc442f81
Durga Yamini <kodamanchiliyamini@gmail.com>
2022-07-15 17:34:38 +0530

    created app and added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BooksComponent } from './components/books/books.component';
+import { SearchComponent } from './components/search/search.component';
+import { SuggestComponent } from './components/suggest/suggest.component';
+import { BookshelfComponent } from './components/bookshelf/bookshelf.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    BooksComponent,
+    SearchComponent,
+    SuggestComponent,
+    BookshelfComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
